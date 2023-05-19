d8cae8281d5682b950ea91f82972279e682e235a
Elliot Markley <Elliotmarkley.dev@gmail.com>
2023-05-18 21:47:25 -0400

    Add get path

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import Express from "express";
-import Mongoose from "mongoose";
+import Mongoose, { Schema } from "mongoose";
 import bodyParser from "body-parser";
 
 const app = Express();
@@ -9,7 +9,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(Express.static("public"));
 
-Mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true, useUnifiedTopology: true });
+Mongoose.connect("mongodb://127.0.0.1:27017/wikiDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
 const articleSchema = new Schema({
     title: String,
@@ -18,7 +18,15 @@ const articleSchema = new Schema({
 
 const Article = Mongoose.model("Article", articleSchema);
 
-
+app.get("/articles", (req, res) => {
+    Article.find()
+        .then((foundArticles) => {
+            res.send(foundArticles);
+        })
+        .catch((err) => {
+            res.send(err);
+        });
+});
 
 app.listen(3000, () => {
     console.log("Server started on port 3000");
